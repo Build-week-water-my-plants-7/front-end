ee841d3e60b39d1c8f0da49a8d6b075c8fa90189
Astrospence <illustrator.wood@gmail.com>
2021-09-20 14:50:41 -0600

    fixed link idea

--- src/components/Plant.js
+++ src/components/Plant.js
@@ -5,11 +5,10 @@ export default function Plant(props) {
     const { nickname, species, h2oFrequency } = props;
     return (
         <div>
-            <Link to="/editplants">
                 <h2>{nickname}</h2>
                 <h4>{species}</h4>
                 <p>{h2oFrequency}</p>
-            </Link>
+            <Link to="/editplants">Edit</Link>
         </div>
     )
 }
